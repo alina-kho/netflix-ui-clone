b7586a9961c203328afaad1de47a03d7d7a30812
alina-kho <alina.khorolskaya0@gmail.com>
2022-08-28 14:07:23 +0200

    Navbar is fixed and transparent

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,6 +5,11 @@ export const Home = () => {
   return (
     <div className="home">
       <Navbar />
+      <img
+        src="https://www.cinematographe.it/wp-content/uploads/2020/08/love-guaranteed.jpg"
+        alt="home banner pic"
+        width="100%"
+      />
     </div>
   );
 };
